546bb89b328b71bca4d032d061e32ff268413e2e
william1993314 <william1993314@gmail.com>
2021-08-15 22:18:13 -0400

    Add index.css to set the appearance of the web page

--- app.js
+++ app.js
@@ -15,24 +15,27 @@ app.use(express.static('public'))
 
 
 //routes setting
+//index page
 app.get('/', (req, res) => {
-    res.render('index',{restaurants:restaurantList.results})
+    res.render('index', {restaurants: restaurantList.results})
+})
+
+//show page
+app.get('/restaurants/:restaurant_id', (req, res) => {
+    const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+    res.render('show', {restaurant: restaurant})
+})
+
+// search function
+app.get('/search', (req, res) => {
+    const keyword = req.query.keyword
+    const restaurants = restaurantList.results.filter(restaurants => {
+        return restaurants.name.toLowerCase().includes(keyword.toLowerCase())
+    })
+    res.render('index', {restaurants: restaurants, keyword: keyword})
 })
 
 //start and listen on the Express listener
 app.listen(port, () => {
     console.log(`express is listening on localhost:${port}`)
-})
-
-app.get('/restaurants/:restaurant_id', (req, res) => {
-    const restaurant = restaurantList.results.find(restaurant=>restaurant.id.toString() === req.params.restaurant_id)
-    res.render('show', { restaurant: restaurant })
-})
-
-app.get('/search', (req,res) => {
-    const keyword = req.query.keyword
-    const restaurants = restaurantList.results.filter(restaurants => {
-        return restaurants.name.toLowerCase().includes(keyword.toLowerCase())
-    })
-    res.render('index', {restaurants: restaurants, keyword:keyword})
 })
