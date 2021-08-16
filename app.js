c7d4d24bb06b267118eef61c87d468e393f9ca2d
william1993314 <william1993314@gmail.com>
2021-08-15 21:58:49 -0400

    Install handlebars as template engine

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 
 const express = require('express')
-
 const app = express()
+const exphbs = require('express-handlebars')
+const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('hello world!')
+// template engine
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+//routes setting
+app.get('/', (req,res) =>{
+    res.render('index')
 })
 
-app.listen(3000, () => {
-    console.log('App is running on http://localhost:3000.')
+//start and listen on the Express listener
+app.listen(port,()=>{
+    console.log(`express is listening on localhost:${port}`)
 })
