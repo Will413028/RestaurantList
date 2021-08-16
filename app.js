c126c1241f8f064f8ec35fd2dda4bc585f0cfd41
william1993314 <william1993314@gmail.com>
2021-08-15 22:27:09 -0400

    Import data from mongodb

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
-
-
+const Restaurant = require('./models/restaurant')
 const app = express()
+const port = 3000
+
+
 mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+//set mongodb
 const db = mongoose.connection
 
 db.on('error', () => {
@@ -15,23 +19,27 @@ db.once('open', () => {
     console.log('mongodb connected!')
 })
 
-// require restaurant data used in the project
-const restaurantList = require('./restaurants.json')
-const port = 3000
+
 
 // template engine
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
-
-// setting static files
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
 
 
 //routes setting
 //index page
 app.get('/', (req, res) => {
-    res.render('index', {restaurants: restaurantList.results})
+    Restaurant.find()
+        .lean()
+        .then(restaurants => res.render('index', { restaurants }))
+        .catch(error => console.log(error))
 })
+
+
+
 
 //show page
 app.get('/restaurants/:restaurant_id', (req, res) => {
