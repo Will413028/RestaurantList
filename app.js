c8b809895eb832ced8b542c3a2f548d17f37721c
william1993314 <william1993314@gmail.com>
2021-08-15 22:32:53 -0400

    Add create restaurant function

--- app.js
+++ app.js
@@ -38,7 +38,28 @@ app.get('/', (req, res) => {
         .catch(error => console.log(error))
 })
 
+// create a new restaurant
+app.get('/restaurants/new', (req, res) => {
+    res.render('new')
+})
 
+app.post('/restaurants', (req, res) => {
+    const restaurant = new Restaurant({
+        name: req.body.name,
+        name_en: req.body.name_en,
+        category: req.body.category,
+        image: req.body.image || "https://www.ristobartwentyfive.com/wp-content/uploads/2019/07/restaurant-food-salat-2.jpg",
+        location: req.body.location || null,
+        google_map: req.body.google_map,
+        rating: req.body.rating,
+        phone: req.body.phone,
+        description: req.body.description
+    })
+
+    restaurant.save()
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
+})
 
 
 //show page
