de080697c165bf9d68a705ded8f906959455fae5
william1993314 <william1993314@gmail.com>
2021-10-23 11:36:50 +0800

    Add message partials

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const session = require('express-session')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 const routes = require('./routes')
 const usePassport = require('./config/passport')
 const Restaurant = require('./models/restaurant')
@@ -37,12 +38,14 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 usePassport(app)
 
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
   next()
 })
-
 // routes setting
 // index page
 app.use(routes)
