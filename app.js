601eb4a9db0504930420e6b418523448afda4548
william1993314 <william1993314@gmail.com>
2021-10-23 09:03:50 +0800

    Add user login feature

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const express = require('express')
-const mongoose = require('mongoose')
-const exphbs = require('express-handlebars')
 const session = require('express-session')
-const Restaurant = require('./models/restaurant')
+const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const routes = require('./routes')
+const usePassport = require('./config/passport')
+const Restaurant = require('./models/restaurant')
 const app = express()
 const port = 3000
-const routes = require('./routes')
+
+
 app.use(methodOverride('_method'))
 app.use(session({
   secret: 'Secret',
   resave: false,
   saveUninitialized: true
 }))
+const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -32,12 +35,11 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+usePassport(app)
 
 // routes setting
 // index page
 app.use(routes)
-
-// create a new restaurant
 
 // start and listen on the Express listener
 app.listen(port, () => {
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('passport')
 const User = require('../../models/user')
 
-router.get('/login', (req,res) =>{
+router.get('/login', (req, res) => {
     res.render('login')
 })
-router.post('/login', (req, res) => {
-})
+
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: 'users/login'
+}))
+
 router.get('/register', (req, res) => {
     res.render('register')
 })
@@ -34,6 +39,5 @@ router.post('/register', (req, res) => {
         }
     })
 })
-
 
 module.exports = router
