e6d20c2174759359e21cb82c62dca90d5c79e5df
william1993314 <william1993314@gmail.com>
2021-10-24 08:50:39 +0800

    Add bcrypt

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
@@ -54,12 +55,15 @@ router.post('/register', (req, res) => {
                 confirmPassword
             })
         }
-        return User.create({
-            name,
-            email,
-            password
-        })
-            .then(() => res.redirect('/'))
+        return bcrypt
+            .genSalt(10)
+            .then(salt => bcrypt.hash(password, salt))
+            .then(hash => User.create({
+                name,
+                email,
+                password:hash
+            }))
+            .then(() =>res.render('/'))
             .catch(err => console.log(err))
     })
 })
